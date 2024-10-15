bb21de6b1c8be952b2062a769d7c6b998d4efeea
Armin <armin.catovic@eqtpartners.com>
2024-10-15 15:47:16 +0200

    added llama translation code

---
+++ pipeline/config.py
@@ -0,0 +1,4 @@
+import os
+
+LLAMA_MODEL_PATH = "mlx-community/Meta-Llama-3.1-8B-Instruct-8bit"
+OPEN_API_KEY = os.getenv("OPEN_API_KEY")
---
+++ pipeline/translation.py
@@ -0,0 +1,30 @@
+from typing import Any
+
+from mlx_lm import generate
+
+from base import set_llama_prompt
+
+def translate_to_english(text: str, model: Any, tokenizer: Any, max_tokens: int = 512) -> str:
+    """
+    Translate the given text to English.
+
+    Args:
+        text (str): The text to translate.
+
+    Returns:
+        str: The translated text.
+    """
+    system_prompt = (
+        "You are an expert language translation assistant, "
+        "tasked with translating online job postings, "
+        "from any given language, into English. "
+        "Don't provide any explanations or additional notes - just translate the given text."
+    )
+    translation_prompt = set_llama_prompt(system_prompt, text)
+
+    return generate(
+        model=model,
+        tokenizer=tokenizer,
+        prompt=translation_prompt,
+        max_tokens=max_tokens,
+    )
